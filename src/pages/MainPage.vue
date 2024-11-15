53576ea71fe2e85f9d9203e2f9fdad5f924fc296
KimJeongu <kjw101147@gmail.com>
2024-11-15 12:00:55 +0900

    :recycle: CardList 리팩토링

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,22 +1,40 @@
 <script setup>
-import Card from '../components/course/CourseCard.vue'
-import { onMounted } from 'vue';
-import useCourse from './useCourse.js'
+import { onMounted, ref } from 'vue';
 import CardList from '../components/course/CardList.vue'
 import Banner from '@/components/banner/Banner.vue';
 import Chatbot from '../components/course/Chatbot.vue';
+import { useAPI } from '@/axios/useAPI';
 
-const { courses } = useCourse();
+const { get } = useAPI();
+const courses = ref([]);
+const loading = ref(true);
 
-onMounted(() => {
+async function fetchCards() {
+    try {
+        const response = await get('/courses/popular');  // 백엔드에서 데이터 가져오기
+        courses.value = response.data;
+        console.log('couses: ', courses.value);
+    } catch (error) {
+        console.error("Failed to fetch cards:", error);
+    } finally {
+        loading.value = false;
+    }
+}
 
-});
+function handleCardButtonClick(cardId) {
+    console.log(`Button clicked on card ID: ${cardId}`);
+}
+
+onMounted(fetchCards);
+
 </script>
 
 <template>
     <Banner/>
     <div class="margin-banner"></div>
-    <CardList/>
+    <h2 class="list-title">인기 있는 클래스</h2>
+    <p class="list-sub-title">사람들이 많이 찾는 클래스를 둘러보세요!</p>
+    <CardList :courses="courses"/>
     <CardList/>
     <CardList/>
     <Chatbot/>
@@ -25,5 +43,9 @@ onMounted(() => {
 <style scoped>
 .margin-banner {
     height: 400px;
+}
+
+.list-sub-title {
+    color: #999;
 }
 </style>
