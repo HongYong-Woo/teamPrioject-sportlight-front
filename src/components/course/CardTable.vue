56e4a4044e287836284c8ebb608924f8342f2325
KimJeongu <kjw101147@gmail.com>
2024-11-18 11:37:03 +0900

    :sparkles: CardTable 컴포넌트 생성

---
+++ src/components/course/CardTable.vue
@@ -0,0 +1,52 @@
+<!-- src/components/CardList.vue -->
+<script setup>
+import Card from './CourseCard.vue';
+
+const props = defineProps({
+    courses: {
+        type: Array,
+    }
+})
+
+</script>
+
+<template>
+    <div class="card-list-container">
+        <!-- <div v-if="loading" class="loading">Loading...</div> -->
+        <div class="card-list">
+            <Card v-for="card in courses" :key="card.id" :nickname="card.nickname" :title="card.title"
+                :address="card.address" :tuition="card.tuition" :discountRate="card.discountRate" :time="card.time"
+                :level="card.level" :category="card.category" :rating="card.rating" :reviewCount="card.reviewCount"
+                :image="card.image" @button-click="handleCardButtonClick(card.id)" class="card"/>
+        </div>
+    </div>
+</template>
+
+
+
+<style scoped>
+.card-list-container {
+    overflow-x: auto;
+    padding: 8px;
+}
+
+.card-list {
+    display: flex;
+    flex-wrap: wrap;
+}
+
+.card-list-container::-webkit-scrollbar {
+    display: none;
+}
+
+.card {
+    width: 20%;
+    min-width: 200px;
+}
+
+.loading {
+    text-align: center;
+    font-size: 1.5rem;
+    color: gray;
+}
+</style>
