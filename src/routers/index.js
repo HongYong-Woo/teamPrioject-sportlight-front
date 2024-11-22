abb98f0ec27b660328304d7e77606963950bdb48
KimJeongu <kjw101147@gmail.com>
2024-11-22 10:31:17 +0900

    :sparkles: 클래스 상세 페이지 라우터 추가

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,13 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
 import courseRouter from './course.js';
 import communityRouter from './community.js';
+import courseDetailRouter from "./courseDetail.js";
 
 const routes = [
     {
         path: '',
         component: () => import('../pages/MainPage.vue')
     },
-    courseRouter, communityRouter
+    courseRouter, communityRouter, courseDetailRouter
 ];
 
 const router = createRouter({
